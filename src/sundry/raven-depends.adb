401f93826b0230249f95d96ec435a395ebc35664
jrmarino <draco@marino.st>
2023-12-15 17:02:41 -0600

    add standalone depends package

---
+++ src/sundry/raven-depends.adb
@@ -0,0 +1,40 @@
+--  SPDX-License-Identifier: ISC
+--  Reference: /License.txt
+
+package body Raven.Depends is
+
+   --------------------------------------------------------------------
+   --  string_to_operator
+   --------------------------------------------------------------------
+   function string_to_operator (instr : String) return Dep_Version_Operator
+   is
+      result : Dep_Version_Operator := VERSION_ANY;
+   begin
+      case instr'Length is
+         when 1 =>
+            if instr = ">" then
+               result := VERSION_GT;
+            elsif instr = "<" then
+               result := VERSION_LT;
+            elsif instr = "!" then
+               result := VERSION_NOT;
+            elsif instr = "=" then
+               result := VERSION_EQ;
+            end if;
+         when 2 =>
+            if instr = ">=" then
+               result := VERSION_GE;
+            elsif instr = "<=" then
+               result := VERSION_LE;
+            elsif instr = "!=" then
+               result := VERSION_NOT;
+            elsif instr = "==" then
+               result := VERSION_EQ;
+            end if;
+         when others =>
+            null;
+      end case;
+      return result;
+   end string_to_operator;
+
+end Raven.Depends;
---
+++ src/sundry/raven-depends.ads
@@ -0,0 +1,36 @@
+--  SPDX-License-Identifier: ISC
+--  Reference: /License.txt
+
+package Raven.Depends is
+
+   type Dep_Version_Operator is
+     (VERSION_ANY,
+      VERSION_EQ,
+      VERSION_GE,
+      VERSION_LE,
+      VERSION_LT,
+      VERSION_GT,
+      VERSION_NOT);
+
+   type Dep_Flag is mod 2 ** 3;
+
+   DEP_FLAG_NORMAL  : constant Dep_Flag := 0;
+   DEP_FLAG_REQUIRE : constant Dep_Flag := 2 ** 0;
+   DEP_FLAG_GLOB    : constant Dep_Flag := 2 ** 1;
+   DEP_FLAG_REGEXP  : constant Dep_Flag := 2 ** 2;
+
+   type Dep_Version_Item is
+      record
+         version : Text;
+         op      : Dep_Version_Operator;
+      end record;
+
+   type Dep_Option_Item is
+      record
+         option : Text;
+         active : Boolean;
+      end record;
+
+   function string_to_operator (instr : String) return Dep_Version_Operator;
+
+end Raven.Depends;
