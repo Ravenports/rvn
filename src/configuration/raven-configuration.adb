4ef3695d88e366a5f1b2d63f22a71f2d02c7bb96
jrmarino <draco@marino.st>
2023-12-22 18:19:02 -0600

    Relocate ABI default

--- src/configuration/raven-configuration.adb
+++ src/configuration/raven-configuration.adb
@@ -6,6 +6,7 @@ with Raven.Event;
 with Ada.Characters.Latin_1;
 with Ada.Environment_Variables;
 with Archive.Unix;
+with Archive.Misc;
 with Ucl;
 With ThickUCL.Files;
 
@@ -191,7 +192,7 @@ package body Raven.Configuration is
          when dbdir          => return "/var/db/" & progname;
          when cachedir       => return "/var/cache/" & progname;
          when keywords_dir   => return "/var/ravenports/conspiracy/Mk/Keywords";
-         when abi            => return "";
+         when abi            => return Archive.Misc.determine_abi;
          when nameserver     => return "";
          when user_agent     => return progname & "/" & progversion;
          when event_pipe     => return "";
