ac1743e447d30222ad98f0f36ada74ed99297cd6
jrmarino <draco@marino.st>
2024-02-18 17:37:47 -0600

    Add the first half the version testsuite

---
+++ tests/frontend/version.sh
@@ -0,0 +1,52 @@
+#! /usr/bin/env atf-sh
+
+. $(atf_get_srcdir)/test_environment.sh
+
+tests_init \
+	version \
+#	compare
+
+version_body() {
+	atf_check -o inline:"<\n" -s exit:0 rvn version -t 1 2
+	atf_check -o inline:">\n" -s exit:0 rvn version -t 2 1
+	atf_check -o inline:"=\n" -s exit:0 rvn version -t 2 2
+	atf_check -o inline:"<\n" -s exit:0 rvn version -t 2 1,1
+	# Special prefixes
+	atf_check -o inline:"<\n" -s exit:0 rvn version -t 1.pl1 1.alpha1
+	atf_check -o inline:"<\n" -s exit:0 rvn version -t 1.alpha1 1.beta1
+	atf_check -o inline:"<\n" -s exit:0 rvn version -t 1.beta1 1.pre1
+	atf_check -o inline:"<\n" -s exit:0 rvn version -t 1.pre1 1.rc1
+	atf_check -o inline:"<\n" -s exit:0 rvn version -t 1.rc1 1
+
+	atf_check -o inline:"<\n" -s exit:0 rvn version -t 1.pl1 1.snap1
+	atf_check -o inline:">\n" -s exit:0 rvn version -t 1.snap1 1.alpha1
+}
+
+compare_body() {
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg test test 5.20_3
+
+	atf_check \
+		-o match:".*Installing.*" \
+		rvn register -M test.ucl
+	atf_check \
+		-o ignore \
+		rvn info "test>0"
+	atf_check \
+		-o ignore \
+		-e ignore \
+		-s exit:1 \
+		rvn info "test<5"
+	atf_check \
+		-o ignore \
+		rvn info "test>5<6"
+	atf_check \
+		-o ignore \
+		-e ignore \
+		-s exit:1 \
+		rvn info "test>5<5.20"
+	atf_check \
+		-o ignore \
+		-e ignore \
+		-s exit:1 \
+		rvn info "test>5.20_3<6"
+}
