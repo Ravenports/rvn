a144194246d259f3040bb8ec4f22018ac1573944
John Marino <draco@marino.st>
2024-05-13 06:47:13 +0000

    Apply new script to update date on man pages
    Read git commit time to set the date on the man pages.
    Right now every time the footers are updated, the
    commit stamp changes on ALL the man pages, but once
    all the pages are created this effect should stop.
    
    The new script is fix-dates.sh

---
+++ manpages/fix-dates.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#
+# Run inside a git repository to update the man page dates.
+
+for F in $(ls *.[1-8])
+do
+   tstamp=$(git log -1 --format=%ct ${F} | xargs -I {} date -r {} +"%B %d, %Y")
+   echo "${F}: ${tstamp}"
+   sed -i'' -e "s/.Dd .*$/.Dd ${tstamp}/" "${F}"
+done
