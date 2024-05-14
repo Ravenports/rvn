4beeee769ae62ffc7a04b2927ff62cfca4c6b960
John Marino <draco@marino.st>
2024-05-14 03:34:05 +0000

    update dates on man pages

--- manpages/fix-dates.sh
+++ manpages/fix-dates.sh
@@ -6,5 +6,5 @@ for F in $(ls *.[1-8])
 do
    tstamp=$(git log -1 --format=%ct ${F} | xargs -I {} date -r {} +"%B %d, %Y")
    echo "${F}: ${tstamp}"
-   sed -i'' -e "s/.Dd .*$/.Dd ${tstamp}/" "${F}"
+   sed -i '' -e "s/.Dd .*$/.Dd ${tstamp}/" "${F}"
 done
