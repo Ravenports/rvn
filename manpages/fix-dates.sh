3e2eb66412d37c2063c8ff12afe1526df4578fcc
John Marino <draco@marino.st>
2024-05-14 03:43:22 +0000

    fix-dates: pass date on penultimate commit, otherwise all manpage pages change dates every time

--- manpages/fix-dates.sh
+++ manpages/fix-dates.sh
@@ -4,7 +4,7 @@
 
 for F in $(ls *.[1-8])
 do
-   tstamp=$(git log -1 --format=%ct ${F} | xargs -I {} date -r {} +"%B %d, %Y")
+   tstamp=$(git log -1 --skip 1 --format=%ct ${F} | xargs -I {} date -r {} +"%B %d, %Y")
    echo "${F}: ${tstamp}"
    sed -i '' -e "s/.Dd .*$/.Dd ${tstamp}/" "${F}"
 done
