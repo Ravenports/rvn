8773bd22b8a9ef31e94a7bac3a68dac00f9448b8
jrmarino <draco@marino.st>
2024-06-05 12:33:44 -0500

    simplify autoremove code with wrapper/core scheme
    I also discovered I forgot to remove "release_active_lock" from version 1.

--- src/commands/20_autoremove/raven-cmd-autoremove.adb
+++ src/commands/20_autoremove/raven-cmd-autoremove.adb
@@ -19,9 +19,8 @@ package body Raven.Cmd.Autoremove is
    function execute_autoremove_command (comline : Cldata) return Boolean
    is
       rdb         : Database.RDB_Connection;
-      toplist     : Pkgtypes.Package_Set.Vector;
-      purge_list  : Pkgtypes.Package_Set.Vector;
-      purge_order : Deinstall.Purge_Order_Crate.Vector;
+      released    : Boolean;
+      succeeded   : Boolean;
       active_lock : LOK.lock_type := LOK.lock_advisory;
 
       function release_active_lock return Boolean is
@@ -36,92 +35,59 @@ package body Raven.Cmd.Autoremove is
          end if;
          return True;
       end release_active_lock;
-
-      function exit_lock return Boolean
-      is
-         released : Boolean;
-      begin
-         released := release_active_lock;
-         OPS.rdb_close (rdb);
-         return released;
-      end exit_lock;
-
-      --  failure ends with rdb closed
-      function activate_lock return Boolean is
-      begin
-         if not LOK.obtain_lock (rdb, active_lock) then
-            case active_lock is
-               when LOK.lock_advisory  => Event.emit_error (LOK.no_adv_lock);
-               when LOK.lock_exclusive => Event.emit_error (LOK.no_exc_lock);
-               when LOK.lock_readonly  => Event.emit_error (LOK.no_read_lock);
-            end case;
-            OPS.rdb_close (rdb);
-            return False;
-         end if;
-         return True;
-      end activate_lock;
-
-      --  failure ends with rdb closed
-      function hard_lock return Boolean is
-      begin
-         if not LOK.upgrade_lock (rdb, LOK.lock_advisory, LOK.lock_exclusive) then
-            Event.emit_error ("Failed lock upgrade");
-            if not exit_lock then
-               Event.emit_error ("Failed lock removal too");
-            end if;
-            return False;
-         end if;
-         return True;
-      end hard_lock;
-
-      --  ends with rdb closed
-      function fully_unlock return Boolean is
-      begin
-         if not LOK.downgrade_lock (rdb, LOK.lock_exclusive, LOK.lock_advisory) then
-            Event.emit_error ("Failed lock downgrade");
-            if not exit_lock then
-               Event.emit_error ("Failed lock removal too");
-            end if;
-            return False;
-         end if;
-         return exit_lock;
-      end fully_unlock;
-
    begin
+      if comline.common_options.dry_run then
+         active_lock := LOK.lock_readonly;
+      end if;
+
       case OPS.rdb_open_localdb (rdb, Database.installed_packages) is
          when RESULT_OK => null;
          when others => return False;
       end case;
 
-      if comline.common_options.dry_run then
-         active_lock := LOK.lock_readonly;
-      end if;
-
-      if not activate_lock then
+      if not LOK.obtain_lock (rdb, active_lock) then
+         case active_lock is
+            when LOK.lock_advisory  => Event.emit_error (LOK.no_adv_lock);
+            when LOK.lock_exclusive => Event.emit_error (LOK.no_exc_lock);
+            when LOK.lock_readonly  => Event.emit_error (LOK.no_read_lock);
+         end case;
+         OPS.rdb_close (rdb);
          return False;
       end if;
 
+      succeeded := execute_autoremove_command_core (rdb, comline);
+
+      released := release_active_lock;
+      OPS.rdb_close (rdb);
+      return succeeded and then released;
+
+   end execute_autoremove_command;
+
+
+   ---------------------------------------
+   --  execute_autoremove_command_core  --
+   ---------------------------------------
+   function execute_autoremove_command_core
+     (rdb     : in out Database.RDB_Connection;
+      comline : Cldata) return Boolean
+   is
+      toplist     : Pkgtypes.Package_Set.Vector;
+      purge_list  : Pkgtypes.Package_Set.Vector;
+      purge_order : Deinstall.Purge_Order_Crate.Vector;
+   begin
       if not DEL.autoremoval_list (rdb, toplist) then
          Event.emit_error ("Failed to retrieve autoremoval list");
-         if exit_lock then
-            null;
-         end if;
          return False;
       end if;
 
       if toplist.Is_Empty then
          if not comline.common_options.quiet then
             Event.emit_message ("No installed packages were selected for autoremoval.");
-            return exit_lock;
+            return True;
          end if;
       end if;
 
       DEL.prune_candidates_with_reverse_deps (rdb, toplist, purge_list);
-      if not release_active_lock then
-         OPS.rdb_close (rdb);
-         return False;
-      end if;
-
       Deinstall.determine_purge_order (purge_list, purge_order);
 
       --  Show removal list unless --quiet is set
@@ -132,12 +98,10 @@ package body Raven.Cmd.Autoremove is
          dryrun       => comline.common_options.dry_run);
 
       if comline.common_options.dry_run then
-         OPS.rdb_close (rdb);
          return True;
       end if;
 
       if not Deinstall.granted_permission_to_proceed then
-         OPS.rdb_close (rdb);
          return True;
       else
          if not comline.common_options.quiet then
@@ -145,7 +109,8 @@ package body Raven.Cmd.Autoremove is
          end if;
       end if;
 
-      if not hard_lock then
+      if not LOK.upgrade_lock (rdb, LOK.lock_advisory, LOK.lock_exclusive) then
+         Event.emit_error ("Failed upgrade to exclusive lock");
          return False;
       end if;
 
@@ -179,8 +144,13 @@ package body Raven.Cmd.Autoremove is
          end;
       end if;
 
-      return fully_unlock;
+      if not LOK.downgrade_lock (rdb, LOK.lock_exclusive, LOK.lock_advisory) then
+         Event.emit_error ("Failed downgrade to advisory lock");
+         return False;
+      end if;
 
-   end execute_autoremove_command;
+      return True;
+
+   end execute_autoremove_command_core;
 
 end Raven.Cmd.Autoremove;
--- src/commands/20_autoremove/raven-cmd-autoremove.ads
+++ src/commands/20_autoremove/raven-cmd-autoremove.ads
@@ -1,9 +1,17 @@
 --  SPDX-License-Identifier: ISC
 --  Reference: /License.txt
 
+private with Raven.Database;
+
 package Raven.Cmd.Autoremove is
 
-   --  Executes autoremove command
+   --  Executes autoremove command (wrapper)
    function execute_autoremove_command (comline : Cldata) return Boolean;
+
+private
+
+   function execute_autoremove_command_core
+     (rdb     : in out Database.RDB_Connection;
+      comline : Cldata) return Boolean;
 
 end Raven.Cmd.Autoremove;
