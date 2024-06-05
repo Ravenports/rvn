f5eeb7547d4ab3c22751bbe42a93d359d3e9eb10
jrmarino <draco@marino.st>
2024-06-05 09:59:33 -0500

    add locks to remove
    Again, the opened database was not closed a couple of times

--- src/commands/19_remove/raven-cmd-remove.adb
+++ src/commands/19_remove/raven-cmd-remove.adb
@@ -3,6 +3,7 @@
 
 with Raven.Event;
 with Raven.Deinstall;
+with Raven.Database.Lock;
 with Raven.Database.Remove;
 with Raven.Database.Operations;
 with Raven.Strings;
@@ -13,6 +14,7 @@ package body Raven.Cmd.Remove is
 
    package OPS renames Raven.Database.Operations;
    package DEL renames Raven.Database.Remove;
+   package LOK renames Raven.Database.Lock;
 
 
    ------------------------------
@@ -25,11 +27,47 @@ package body Raven.Cmd.Remove is
       toplist     : Pkgtypes.Package_Set.Vector;
       purge_list  : Pkgtypes.Package_Set.Vector;
       purge_order : Deinstall.Purge_Order_Crate.Vector;
+      active_lock : LOK.lock_type := LOK.lock_readonly;
+
+      procedure release_active_lock is
+      begin
+         if not LOK.release_lock (rdb, active_lock) then
+            case active_lock is
+               when LOK.lock_advisory  => Event.emit_error (LOK.no_advisory_unlock);
+               when LOK.lock_exclusive => Event.emit_error (LOK.no_exclusive_unlock);
+               when LOK.lock_readonly  => Event.emit_error (LOK.no_read_unlock);
+            end case;
+         end if;
+      end release_active_lock;
+
+      procedure exit_lock is
+      begin
+         release_active_lock;
+         OPS.rdb_close (rdb);
+      end exit_lock;
+
+      function activate_lock return Boolean is
+      begin
+         if not LOK.obtain_lock (rdb, active_lock) then
+            case active_lock is
+               when LOK.lock_advisory  => Event.emit_error (LOK.no_adv_lock);
+               when LOK.lock_exclusive => Event.emit_error (LOK.no_exc_lock);
+               when LOK.lock_readonly  => Event.emit_error (LOK.no_read_lock);
+            end case;
+            OPS.rdb_close (rdb);
+            return False;
+         end if;
+         return True;
+      end activate_lock;
    begin
       case OPS.rdb_open_localdb (rdb, Database.installed_packages) is
          when RESULT_OK => null;
          when others => return False;
       end case;
+
+      if not activate_lock then
+         return False;
+      end if;
 
       if comline.common_options.all_installed_pkgs then
          success := DEL.top_level_deletion_list
@@ -60,12 +98,14 @@ package body Raven.Cmd.Remove is
       end if;
 
       if not success then
+         exit_lock;
          return False;
       end if;
 
       if toplist.Is_Empty then
          if not comline.common_options.quiet then
             Event.emit_message ("No installed packages were selected for removal.");
+            exit_lock;
             return True;
          end if;
       end if;
@@ -76,6 +116,7 @@ package body Raven.Cmd.Remove is
          purge_list   => purge_list,
          force        => comline.cmd_remove.force_breakage);
 
+      release_active_lock;
       Deinstall.determine_purge_order (purge_list, purge_order);
 
       --  Show removal list unless --quiet is set
@@ -86,15 +127,22 @@ package body Raven.Cmd.Remove is
          dryrun       => comline.common_options.dry_run);
 
       if comline.common_options.dry_run then
+         OPS.rdb_close (rdb);
          return True;
       end if;
 
       if not Deinstall.granted_permission_to_proceed then
+         OPS.rdb_close (rdb);
          return True;
       else
          if not comline.common_options.quiet then
             Event.emit_message ("");
          end if;
+      end if;
+
+      active_lock := LOK.lock_advisory;
+      if not activate_lock then
+         return False;
       end if;
 
       Deinstall.remove_packages_in_order
@@ -105,7 +153,7 @@ package body Raven.Cmd.Remove is
          skip_scripts => comline.cmd_remove.inhibit_scripts,
          quiet        => comline.common_options.quiet);
 
-      OPS.rdb_close (rdb);
+      exit_lock;
       return success;
    end execute_remove_command;
 
