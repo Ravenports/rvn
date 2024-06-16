2ffaeefc818639d132270c6bbbe1b915c5081954
jrmarino <draco@marino.st>
2024-06-16 17:16:51 -0500

    finished messages tests

--- tests/frontend/messages.sh
+++ tests/frontend/messages.sh
@@ -4,7 +4,13 @@
 
 tests_init \
 	messages \
-	messages_install
+	messages_install \
+	messages_below_lower_threshold_upgrade \
+	messages_above_lower_threshold_upgrade \
+	messages_exact_upper_threshold_upgrade \
+	messages_above_upper_threshold_upgrade \
+	messages_no_threshold_upgrade
+
 
 test_setup () {
 	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "single" "standard" "5.20_3" "/"
@@ -13,9 +19,9 @@ messages: [
 	{ message: "package being removed", type: "remove" },
 	{ message: "package being installed", type: "install" },
 	{ message: "package is being upgraded", type: "upgrade" },
-	{ message: "Upgrading from lower than 1.0", maximum_version: "1.0", type: "upgrade" },
-	{ message: "Upgrading from higher than 1.0", minimum_version: "1.0", type: "upgrade"  },
-	{ message: "Upgrading from >1.0 < 3.0", maximum_version: "3.0", minimum_version: "1.0", type: "upgrade"  }
+	{ message: "Upgrading from lower than 1.0", max_version: "1.0", type: "upgrade" },
+	{ message: "Upgrading from higher than 1.0", min_version: "1.0", type: "upgrade"  },
+	{ message: "Upgrading from >= 1.0 <= 3.0", max_version: "3.0", min_version: "1.0", type: "upgrade"  }
 ]
 EOF
 
@@ -60,93 +66,181 @@ messages_install_body() {
 		-o match:"^test-single-standard installation messages" \
 		-o match:"^package being installed$" \
 		-e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . install -qy test
-
-
 }
 
-dummy() {
+messages_below_lower_threshold_upgrade_body() {
+touch dummy.plist
+	mkdir target
+	mkdir files
 
+	test_setup
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w dummy.plist
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test091" "test" "single" "standard" "0.91" "/"
+cat <<EOF >> test091.ucl 
+messages: [
+	{ message: "package 0.91 being removed", type: "remove" }
+]
+EOF
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR} -r . -m test091.ucl -w dummy.plist
+
+	atf_check -o ignore \
+		-e empty -s exit:0 rvn -r ${TMPDIR}/target install -qy --file ${TMPDIR}/test-single-standard-0.91.rvn
+
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	# don't show deinstallation messages of old package when upgrading
+	atf_check \
+		-o match:"^test-single-standard upgrade messages" \
+		-o match:"^package is being upgraded$" \
+		-o match:"^Upgrading from lower than 1.0$" \
+		-e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . upgrade -qy test
+}
+
+messages_above_lower_threshold_upgrade_body() {
+	touch dummy.plist
+	mkdir target
+	mkdir files
+
+	test_setup
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w dummy.plist
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test151" "test" "single" "standard" "1.51" "/"
+cat <<EOF >> test151.ucl 
+messages: [
+	{ message: "package 1.51 being removed", type: "remove" }
+]
+EOF
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR} -r . -m test151.ucl -w dummy.plist
+
+	atf_check -o ignore \
+		-e empty -s exit:0 rvn -r ${TMPDIR}/target install -qy --file ${TMPDIR}/test-single-standard-1.51.rvn
+
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	# don't show deinstallation messages of old package when upgrading
+	atf_check \
+		-o match:"^test-single-standard upgrade messages" \
+		-o match:"^package is being upgraded$" \
+		-o match:"^Upgrading from higher than 1.0$" \
+		-o match:"^Upgrading from >= 1.0 <= 3.0$" \
+		-e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . upgrade -qy test
+}
+
+messages_exact_upper_threshold_upgrade_body() {
+	touch dummy.plist
+	mkdir target
+	mkdir files
+
+	test_setup
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w dummy.plist
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test300" "test" "single" "standard" "3" "/"
+cat <<EOF >> test300.ucl 
+messages: [
+	{ message: "package 3.00 being removed", type: "remove" }
+]
+EOF
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR} -r . -m test300.ucl -w dummy.plist
+
+	atf_check -o ignore \
+		-e empty -s exit:0 rvn -r ${TMPDIR}/target install -qy --file ${TMPDIR}/test-single-standard-3.rvn
+
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	# don't show deinstallation messages of old package when upgrading
+	atf_check \
+		-o match:"^test-single-standard upgrade messages" \
+		-o match:"^package is being upgraded$" \
+		-o match:"^Upgrading from higher than 1.0$" \
+		-o match:"^Upgrading from >= 1.0 <= 3.0$" \
+		-e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . upgrade -qy test
+}
+
+messages_above_upper_threshold_upgrade_body() {
+	touch dummy.plist
+	mkdir target
+	mkdir files
+
+	test_setup
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w dummy.plist
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test301" "test" "single" "standard" "3.01" "/"
+cat <<EOF >> test301.ucl 
+messages: [
+	{ message: "package 3.00 being removed", type: "remove" }
+]
+EOF
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR} -r . -m test301.ucl -w dummy.plist
+
+	atf_check -o ignore \
+		-e empty -s exit:0 rvn -r ${TMPDIR}/target install -qy --file ${TMPDIR}/test-single-standard-3.01.rvn
+
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	# don't show deinstallation messages of old package when upgrading
+	atf_check \
+		-o match:"^test-single-standard upgrade messages" \
+		-o match:"^package is being upgraded$" \
+		-o match:"^Upgrading from higher than 1.0$" \
+		-e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . upgrade -qy test
+}
+
+messages_no_threshold_upgrade_body() {
+	touch dummy.plist
+	mkdir target
+	mkdir files
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "single" "standard" "5.20_3" "/"
+	cat <<EOF >> test.ucl 
+messages: [
+	{ message: "package being removed", type: "remove" },
+	{ message: "package being installed", type: "install" },
+	{ message: "package is being upgraded", type: "upgrade" },
+]
+EOF
 
 	mkdir reposconf
-	cat << EOF > reposconf/repo1.conf
-local1: {
-	url: file://${TMPDIR},
+	cat <<EOF >> reposconf/repo.conf
+local: {
+	url: file:///${TMPDIR},
 	enabled: true
 }
 EOF
-	cat > test2.ucl << EOF
-name: "test"
-origin: "test"
-version: "0.20_3"
-arch: "*"
-maintainer: "none"
-prefix: "/usr/local"
-www: "unknown"
-comment: "need one"
-desc: "also need one"
-EOF
-	atf_check -o ignore pkg register -M test2.ucl
-	atf_check -o ignore pkg create -M test.ucl
-	atf_check -o ignore pkg repo .
-	atf_check -o match:"^Upgrading from lower than 1.0.*" \
-	    pkg -o REPOS_DIR="${TMPDIR}/reposconf" -o PKG_CACHEDIR="${TMPDIR}" upgrade -y
-	atf_check -o ignore pkg delete -y test
 
-	cat > test2.ucl << EOF
-name: "test"
-origin: "test"
-version: "4.20_3"
-arch: "*"
-maintainer: "none"
-prefix: "/usr/local"
-www: "unknown"
-comment: "need one"
-desc: "also need one"
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w dummy.plist
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test301" "test" "single" "standard" "3.01" "/"
+cat <<EOF >> test301.ucl 
+messages: [
+	{ message: "package 3.00 being removed", type: "remove" }
+]
 EOF
-	atf_check -o ignore pkg register -M test2.ucl
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR} -r . -m test301.ucl -w dummy.plist
+
+	atf_check -o ignore \
+		-e empty -s exit:0 rvn -r ${TMPDIR}/target install -qy --file ${TMPDIR}/test-single-standard-3.01.rvn
+
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	# don't show deinstallation messages of old package when upgrading
 	atf_check \
-		-o match:"^Upgrading from higher than 1.0.*" \
-	    pkg -o REPOS_DIR="${TMPDIR}/reposconf" -o PKG_CACHEDIR="${TMPDIR}" upgrade -y
-	atf_check -o ignore pkg delete -y test
-
-	cat > test2.ucl << EOF
-name: "test"
-origin: "test"
-version: "2.20_3"
-arch: "*"
-maintainer: "none"
-prefix: "/usr/local"
-www: "unknown"
-comment: "need one"
-desc: "also need one"
-EOF
-	atf_check -o ignore pkg register -M test2.ucl
-	atf_check \
-		-o match:"^Upgrading from >1.0 < 3.0.*" \
-		-o match:"^Upgrading from higher than 1.0.*" \
-		pkg -o REPOS_DIR="${TMPDIR}/reposconf" -o PKG_CACHEDIR="${TMPDIR}" upgrade -y
-OUTPUT='test-5.20_3:
-Always:
-Always print
-
-On remove:
-package being removed
-
-On install:
-package being installed
-
-On upgrade:
-package is being upgraded
-
-On upgrade from test<1.0:
-Upgrading from lower than 1.0
-
-On upgrade from test>1.0:
-Upgrading from higher than 1.0
-
-On upgrade from test>1.0<3.0:
-Upgrading from >1.0 < 3.0
-
-'
-	atf_check -o inline:"${OUTPUT}" pkg info -D test
+		-o match:"^test-single-standard upgrade messages" \
+		-o match:"^package is being upgraded$" \
+		-e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . upgrade -qy test
 }
+
