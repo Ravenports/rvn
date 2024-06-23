43d5803578bbcd8968398180339c398ad91e472b
jrmarino <draco@marino.st>
2024-06-23 09:09:23 -0500

    finish trigger test suite
    everything is working as expected.
    I might add a "@dir" + dir_path test.

--- tests/frontend/triggers.sh
+++ tests/frontend/triggers.sh
@@ -5,13 +5,12 @@
 tests_init \
 	cleanup_lua \
 	dirpath \
-	glob_trigger
-
-#	 \
-#	regex_trigger \
-#	path_trigger \
-#	pkg_exec_no_sandbox \
-#	pkg_add
+	glob_trigger \
+	regex_trigger \
+	path_trigger \
+	no_trigger \
+	swap_order \
+	upgrade
 
 
 #################
@@ -135,135 +134,275 @@ EOF
 
 	atf_check -o inline:"arg 1: /share/hello.cpp\narg 2: /share/hello.doc\narg 3: /share/hello.txt\norigin namebase   : test\norigin subpackage : single\norigin variant    : standard\norigin prefix     : /\n" \
 		-e empty -s exit:0 rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy test
-
 }
 
 
+###################
+#  regex_trigger  #
+###################
+regex_trigger_body() {
+	mkdir files
+	mkdir target
+	mkdir share
+	echo "hello" > share/hello.txt
+	echo "bob" > share/hello.cpp
+	echo "jim" > share/hello.doc
+	echo "share/hello.cpp" > test.plist
+	echo "share/hello.txt" >> test.plist
+	echo "share/hello.doc" >> test.plist
+	mkdir reposconf
+	cat <<EOF >> reposconf/repo.conf
+local: {
+	url: file:///${TMPDIR},
+	enabled: true
+}
+EOF
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "single" "standard" "1" "/"
+	cat <<EOF >> test.ucl
+triggers: [
+  {
+    file_regexp: ["hello[.][a-z]{3}"]
+    trigger: <<EOS
+  for i,v in ipairs(arg) do
+    print("arg " .. i .. ": " .. v)
+  end
+EOS
+  }
+]
+EOF
 
 
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w test.plist
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
 
+	atf_check -o inline:"arg 1: /share/hello.cpp\narg 2: /share/hello.doc\narg 3: /share/hello.txt\n" \
+		-e empty -s exit:0 rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy test
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 ##################
 #  path_trigger  #
 ##################
 path_trigger_body() {
-	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "1" "/"
-	mkdir trigger_dir/
-	cat << EOF >> trigger_dir/trigger.ucl
-path: [ "${TMPDIR}/trigger_dir" ]
-trigger: {
-	type: lua
-	script: <<EOS
-pkg.print_msg("message-test")
-print("triggered " .. arg[1])
-EOS
-}
-EOF
-	echo ${TMPDIR}/trigger_dir/trigger.ucl > plist
-	atf_check pkg create -M test.ucl -p plist
+	# trigger in the prequel, activated with the second install
+	mkdir files
 	mkdir target
-	unset PKG_TRIGGERS_DIR
-	atf_check -o inline:"triggered ${TMPDIR}/trigger_dir\n" pkg -o REPOS_DIR=/dev/null -o PKG_TRIGGERS_DIR="${TMPDIR}/trigger_dir" install -qfy ${TMPDIR}/test-1.pkg
+	mkdir share
+	mkdir bin
+	echo "hello" > share/hello.txt
+	echo "bob" > share/hello.cpp
+	echo "jim" > share/hello.doc
+	echo "echo thursday" > bin/dow.sh
+	echo "share/hello.cpp" > test.plist
+	echo "share/hello.txt" >> test.plist
+	echo "share/hello.doc" >> test.plist
+	echo "bin/dow.sh" > prequel.plist
+	mkdir reposconf
+	cat <<EOF >> reposconf/repo.conf
+local: {
+	url: file:///${TMPDIR},
+	enabled: true
 }
+EOF
 
-
-
-
-
-
-
-
-
-regex_trigger_body() {
-	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "1" "/"
-	mkdir trigger_dir/
-	cat << EOF >> trigger_dir/trigger.ucl
-path_regex: [ ".*trigger.*" ]
-trigger: {
-	type: lua
-	script: <<EOS
-	print("triggered " .. arg[1])
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "single" "standard" "1" "/"
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "prequel" "prequel" "single" "standard" "1" "/"
+	cat <<EOF >> prequel.ucl
+triggers: [
+  {
+    file_path: ["/share/hello.cpp"]
+    trigger: <<EOS
+  pkg.print_msg("origin rootdir: " .. pkg_rootdir)
+  for i,v in ipairs(arg) do
+    print("arg " .. i .. ": " .. v)
+  end
 EOS
+  }
+]
+EOF
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w test.plist
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m prequel.ucl -w prequel.plist
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	atf_check -o empty -e empty -s exit:0 rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy prequel
+	atf_check \
+		-o match:"^arg 1: /share/hello.cpp$" \
+		-o match:"^origin rootdir: .*/work/target$" \
+		-e empty -s exit:0 rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy test
+
+}
+
+
+
+################
+#  no_trigger  #
+################
+no_trigger_body() {
+	# do not trigger if matching file is already installed
+	mkdir files
+	mkdir target
+	mkdir share
+	mkdir bin
+	echo "hello" > share/hello.txt
+	echo "bob" > share/hello.cpp
+	echo "jim" > share/hello.doc
+	echo "echo thursday" > bin/dow.sh
+	echo "share/hello.cpp" > test.plist
+	echo "share/hello.txt" >> test.plist
+	echo "share/hello.doc" >> test.plist
+	echo "bin/dow.sh" > prequel.plist
+	mkdir reposconf
+	cat <<EOF >> reposconf/repo.conf
+local: {
+	url: file:///${TMPDIR},
+	enabled: true
 }
 EOF
-	echo ${TMPDIR}/trigger_dir/trigger.ucl > plist
-	atf_check pkg create -M test.ucl -p plist
-	mkdir target
-	unset PKG_TRIGGERS_DIR
-	atf_check -o inline:"triggered ${TMPDIR}/trigger_dir\n" pkg -o REPOS_DIR=/dev/null -o PKG_TRIGGERS_DIR="${TMPDIR}/trigger_dir" install -qfy ${TMPDIR}/test-1.pkg
-}
 
-
-
-
-pkg_exec_no_sandbox_body() {
-	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "1" "/"
-	mkdir trigger_dir/
-	cat << EOF >> trigger_dir/trigger.ucl
-path_glob: [ "/*" ]
-trigger: {
-	type: lua
-	sandbox: false
-	script: <<EOS
-pkg.exec({"echo", "plop"})
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "single" "standard" "1" "/"
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "prequel" "prequel" "single" "standard" "1" "/"
+	cat <<EOF >> test.ucl
+triggers: [
+  {
+    dir_path: ["/bin"]
+    trigger: <<EOS
+  for i,v in ipairs(arg) do
+    print("arg " .. i .. ": " .. v)
+  end
 EOS
-}
+  }
+]
 EOF
-	echo ${TMPDIR}/trigger_dir/trigger.ucl > plist
-	atf_check pkg create -M test.ucl -p plist
-	mkdir target
-	unset PKG_TRIGGERS_DIR
-	atf_check -o inline:"plop\n" pkg -o REPOS_DIR=/dev/null -o PKG_TRIGGERS_DIR="${TMPDIR}/trigger_dir" install -qfy ${TMPDIR}/test-1.pkg
+
+atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w test.plist
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m prequel.ucl -w prequel.plist
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	atf_check -o empty -e empty -s exit:0 rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy prequel
+	atf_check -o empty -e empty -s exit:0 rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy test
 }
 
-pkg_add_body() {
-	atf_check -s exit:0 ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "1" "/"
-	mkdir trigger_dir/
-	cat << EOF >> trigger_dir/trigger.ucl
-path_glob: [ "/*" ]
-trigger: {
-	type: lua
-	sandbox: false
-	script: <<EOS
-print("plop")
+
+
+
+################
+#  swap_order  #
+################
+swap_order_body() {
+	# do not trigger if matching file is already installed
+	mkdir files
+	mkdir target
+	mkdir share
+	mkdir bin
+	echo "hello" > share/hello.txt
+	echo "bob" > share/hello.cpp
+	echo "jim" > share/hello.doc
+	echo "echo thursday" > bin/dow.sh
+	echo "share/hello.cpp" > test.plist
+	echo "share/hello.txt" >> test.plist
+	echo "share/hello.doc" >> test.plist
+	echo "bin/dow.sh" > prequel.plist
+	mkdir reposconf
+	cat <<EOF >> reposconf/repo.conf
+local: {
+	url: file:///${TMPDIR},
+	enabled: true
+}
+EOF
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test" "test" "single" "standard" "1" "/"
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "prequel" "prequel" "single" "standard" "1" "/"
+	cat <<EOF >> test.ucl
+triggers: [
+  {
+    dir_path: ["/bin"]
+    trigger: <<EOS
+  for i,v in ipairs(arg) do
+    print("arg " .. i .. ": " .. v)
+  end
 EOS
+  }
+]
+EOF
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test.ucl -w test.plist
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m prequel.ucl -w prequel.plist
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	atf_check -o empty -e empty -s exit:0 rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy test
+	atf_check -o inline:"arg 1: /bin\n" -e empty -s exit:0 rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy prequel
+}
+
+
+#############
+#  upgrade  #
+#############
+upgrade_body() {
+	mkdir files
+	mkdir target
+	mkdir bin
+	echo "echo thursday" > bin/dow.sh
+	echo "echo friday, I'm in love." > bin/love.sh
+	echo "bin/dow.sh" > test1.plist
+	echo "bin/dow.sh" > test2.plist
+	echo "bin/love.sh" >> test2.plist
+	mkdir reposconf
+	cat <<EOF >> reposconf/repo.conf
+local: {
+	url: file:///${TMPDIR},
+	enabled: true
 }
 EOF
-	echo "${TMPDIR}"/trigger_dir/trigger.ucl > plist
-	atf_check pkg create -M test.ucl -p plist
 
-	atf_check -s exit:0 ${RESOURCEDIR}/test_subr.sh new_pkg "meh" "meh" "1" "/"
-	cat << EOF >> meh.ucl
-deps: { test: { version: "1", origin: "dontcare" } }
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test1" "test" "single" "standard" "1" "/"
+	cat <<EOF >> test1.ucl
+triggers: [
+  {
+    dir_path: ["/bin"]
+    trigger: <<EOS
+if pkg_upgrade then
+   print "bin directory hit, upgraded"
+else
+   print "bin directory hit, installed"
+end
+EOS
+  }
+]
 EOF
-	echo "@dir ${TMPDIR}"/trigger_dir/ > plist
-	mkdir target
-	unset PKG_TRIGGERS_DIR
-	atf_check pkg create -M meh.ucl -p plist
-OUTPUT="Installing meh-1...
-\`-- Installing test-1...
-\`-- Extracting test-1:  done
-Extracting meh-1:  done
-==> Running trigger: trigger.ucl
-plop
-"
-	atf_check -o inline:"${OUTPUT}" pkg -o PKG_TRIGGERS_DIR="${TMPDIR}/trigger_dir" add meh-1.pkg
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test1.ucl -w test1.plist
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+	atf_check -o inline:"bin directory hit, installed\n" -e empty -s exit:0 \
+		rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" install -qy test
+
+	rm ${TMPDIR}/files/test-single-standard-1.rvn
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "test2" "test" "single" "standard" "2" "/"
+	cat <<EOF >> test2.ucl
+triggers: [
+  {
+    dir_path: ["/bin"]
+    trigger: <<EOS
+if pkg_upgrade then
+   print "bin directory hit, upgraded"
+else
+   print "bin directory hit, installed"
+end
+EOS
+  }
+]
+EOF
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m test2.ucl -w test2.plist
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+	atf_check -o inline:"bin directory hit, upgraded\n" -e empty -s exit:0 \
+		rvn -r ${TMPDIR}/target -o REPOS_DIR="${TMPDIR}/reposconf" upgrade -qy test
 }
