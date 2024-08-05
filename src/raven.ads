5fd737e950c1f64c6194adcb320c17462813d9a6
jrmarino <draco@marino.st>
2024-08-05 08:55:43 -0500

    bump version for midnight setup script

--- src/raven.ads
+++ src/raven.ads
@@ -7,7 +7,7 @@ package Raven is
 
    package SU renames Ada.Strings.Unbounded;
    
-   progversion    : constant String := "0.3.1";
+   progversion    : constant String := "0.3.2";
    progname       : constant String := "rvn";
    extension      : constant String := ".rvn";
    install_loc    : constant String := "/raven";
