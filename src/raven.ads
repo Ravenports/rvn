0ec464d0fd8817a0d954f2ccc2f5b2572d09a9c3
jrmarino <draco@marino.st>
2024-08-13 20:15:03 -0500

    bump version for eventually release

--- src/raven.ads
+++ src/raven.ads
@@ -7,7 +7,7 @@ package Raven is
 
    package SU renames Ada.Strings.Unbounded;
    
-   progversion    : constant String := "0.3.3";
+   progversion    : constant String := "0.3.4";
    progname       : constant String := "rvn";
    extension      : constant String := ".rvn";
    install_loc    : constant String := "/raven";
