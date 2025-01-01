6efb2e422092e059e73a6a809cf95592eec8c3da
jrmarino <draco@marino.st>
2025-01-01 10:28:40 -0600

    Bump version for sync with rvn-format 0.4.0
    This requires a re-tag (fine since no RP release has happened yet)

--- src/raven.ads
+++ src/raven.ads
@@ -7,7 +7,7 @@ package Raven is
 
    package SU renames Ada.Strings.Unbounded;
    
-   progversion    : constant String := "0.3.9";
+   progversion    : constant String := "0.4.0";
    progname       : constant String := "rvn";
    extension      : constant String := ".rvn";
    install_loc    : constant String := "/raven";
