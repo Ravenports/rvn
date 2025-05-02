06089b040b765686bb2cfa12c43f65f80a01338f
jrmarino <draco@marino.st>
2025-05-02 06:40:50 -0500

    Start new test case for issue #12

---
+++ tests/frontend/issue12.sh
@@ -0,0 +1,142 @@
+#! /usr/bin/env atf-sh
+
+. $(atf_get_srcdir)/test_environment.sh
+
+tests_init replace_library
+
+
+gencfiles() {
+	cat << EOF >> mylibrary.h
+#ifndef MYLIBRARY_H
+#define MYLIBRARY_H
+
+int add(int a, int b);
+void print_hello();
+
+#endif
+EOF
+
+	cat << EOF >> mylibrary.c
+#include "mylibrary.h"
+#include <stdio.h> // For printf
+
+int add(int a, int b) {
+   return a + b;
+}
+
+void print_hello() {
+   printf("Hello from mylibrary!\n");
+}
+EOF
+
+	cat << EOF >> mylibrary2.c
+#include "mylibrary.h"
+#include <stdio.h> // For printf
+
+int add(int a, int b) {
+   return a + b;
+}
+
+void print_hello() {
+   printf("Hello from mylibrary version 2.0!\n");
+}
+EOF
+
+	cat << EOF >> main.c
+#include <stdio.h>
+#include "mylibrary.h"
+
+int main() {
+    print_hello();
+    int result = add(5, 3);
+    printf("Result: %d\n", result);
+    return 5;
+}
+EOF
+}
+
+gencheck() {
+	cat << EOF >> get_soname.sh
+#!/bin/sh
+readelf -d \$1 | grep SONAME
+EOF
+}
+
+replace_library_body() {
+	touch dummy.plist
+	mkdir -p ${TMPDIR}/target/var/cache/rvn
+	mkdir files
+	mkdir target
+	mkdir reposconf
+	cat <<EOF >> reposconf/repo.conf
+local: {
+	url: file:///${TMPDIR},
+	enabled: true
+}
+EOF
+
+	cat << EOF >> lib.plist
+libadd.so
+libadd.so.1
+EOF
+
+	cat << EOF >> bin.plist
+helloworld
+EOF
+
+	gencfiles
+	gencheck
+	cc -fPIC -c mylibrary.c -o mylibrary.o
+	cc -shared -Wl,-soname,libadd.so.1 -o libadd.so.1 mylibrary.o
+	ln -s libadd.so.1 libadd.so
+	atf_check \
+		-o match:".*Library soname: \[libadd.so.1\]$" \
+		-s exit:0 /bin/sh get_soname.sh "libadd.so.1"
+
+	cc main.c -L${TMPDIR} -Wl,-rpath,${TMPDIR} -ladd -o ${TMPDIR}/helloworld
+	atf_check -o inline:"Hello from mylibrary!\nResult: 8\n" -s exit:5 ${TMPDIR}/helloworld
+
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "lib1" "mylib" "single" "standard" "1" "/"
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "lib2" "mylib" "single" "standard" "2" "/"
+	atf_check -s exit:0 sh ${RESOURCEDIR}/test_subr.sh new_pkg "binary" "helloworld" "single" "standard" "1" "/"
+
+	cat << EOF >> ${TMPDIR}/binary.ucl
+deps: {"mylib~single~standard": "1"}
+EOF
+
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m lib1.ucl -w lib.plist
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m binary.ucl -w bin.plist
+
+	# generate first version of the repository
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	# install helloworld
+	atf_check \
+		-o ignore \
+		-e empty \
+		-s exit:0 \
+		rvn -R "${TMPDIR}/reposconf" -r ${TMPDIR}/target install -U -y helloworld
+
+	# ./target/helloworld
+	# confirmed what it installed
+	atf_check -o inline:"mylib~single~standard~1\nhelloworld~single~standard~1\n" \
+		-e empty -s exit:0 rvn -r . info -q
+
+	# replace library package and rebuild helloworld
+	rm ${TMPDIR}/files/mylib~single~standard~1.rvn
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m lib2.ucl -w lib.plist
+
+	sed -i'' -e '/mylib/ s|1|2|' ${TMPDIR}/binary.ucl
+	cat ${TMPDIR}/binary.ucl
+
+	# regenerate helloworld package
+	atf_check -o empty -e empty -s exit:0 rvn create -o ${TMPDIR}/files -r . -m binary.ucl -w bin.plist
+
+	# regen repository
+	atf_check -o empty -e empty -s exit:0 rvn -r . genrepo --quiet ${TMPDIR}
+	atf_check -o ignore -e empty -s exit:0 rvn -R "${TMPDIR}/reposconf" -r . catalog -f
+
+	rvn -R "${TMPDIR}/reposconf" -r ${TMPDIR}/target upgrade -U -y
+	false
+}
