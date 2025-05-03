853263101265af1ca11f020774dec99cdcf659ec
jrmarino <draco@marino.st>
2025-05-03 14:07:52 -0500

    Release 1.0.0
    It's finally time to crown a release as 1.0.0.
    All commands are implemented and all known issues
    have been solved.

--- src/raven.ads
+++ src/raven.ads
@@ -7,7 +7,7 @@ package Raven is
 
    package SU renames Ada.Strings.Unbounded;
    
-   progversion    : constant String := "0.4.8";
+   progversion    : constant String := "1.0.0";
    progname       : constant String := "rvn";
    extension      : constant String := ".rvn";
    install_loc    : constant String := "/raven";
